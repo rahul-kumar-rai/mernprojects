2bec722580ee303421780778a41604bb6fc482ed
rahul-kumar-rai <rk199440@gmail.com>
2026-07-17 16:04:43 +0530

    basic mern

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
-import { app } from "./App.js";
+import express from "express";
+import cors from "cors";
+import authRouters from "./routers/auth.routes.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+// Create an instance of the Express application
+export const app = express();
+
+
+
+// Middleware
+app.use(cors({
+    origin: "http://localhost:3000", // Allow requests from the specified origin or frontend URL
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/api/auth", authRouters)
+
+
 
 
 connectDB();
@@ -16,3 +35,4 @@ if(process.env.NODE_ENV === "developments"){
     
     });
 }
+
