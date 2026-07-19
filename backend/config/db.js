5122b7558ef64896900824044660fa7472d6b6bd
rahul-kumar-rai <rk199440@gmail.com>
2026-07-19 17:26:46 +0530

    done

--- backend/config/db.js
+++ backend/config/db.js
@@ -18,7 +18,7 @@ const connectDB = async () => {
     const opts = {
       bufferCommands: false,
       maxPoolSize: 10, // Maintain up to 10 socket connections
-      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+      serverSelectionTimeoutMS: 10000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     };
 
